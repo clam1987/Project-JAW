0445461dab66d9a034cd515273e879cc9a3a7754
Cuong Lam <lin-1987@live.com>
2019-04-02 13:34:48 -0700

    updated java auth

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,43 +1,77 @@
 //Setup database
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCvExqwK1-Oq_3316MRntkTb3UVzx4UqwI",
-    authDomain: "fooduserdatabase.firebaseapp.com",
-    databaseURL: "https://fooduserdatabase.firebaseio.com",
-    projectId: "fooduserdatabase",
-    storageBucket: "fooduserdatabase.appspot.com",
-    messagingSenderId: "225180646639"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCvExqwK1-Oq_3316MRntkTb3UVzx4UqwI",
+  authDomain: "fooduserdatabase.firebaseapp.com",
+  databaseURL: "https://fooduserdatabase.firebaseio.com",
+  projectId: "fooduserdatabase",
+  storageBucket: "fooduserdatabase.appspot.com",
+  messagingSenderId: "225180646639"
+};
+firebase.initializeApp(config);
 
-  
+
 
 //initialize functions
 
 
 
 //variables
-var ingridients = $("#searchBar").val().trim();
-var appid = "19be36c3";
-var appkey = "6dae3e55f3c1da568e7fa055321c944f";
-var queryURL = "https://api.edamam.com/search?q=" + ingridients + "&app_id=" + appid + "&app_key=" + appkey;
-console.log(ingridients)
 
 
 
-//function
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response){
-  console.log(response);
+
+
+var fbauth = {
+
+  createUser: function (username, password) {
+    console.log(username)
+    console.log(password)
+    firebase.auth().createUserWithEmailAndPassword(username, password).catch(function (error)  {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(error);
+    });
+  },
+  
+  signIn: function(username, password) {
+  firebase.auth().signInWithEmailAndPassword(username, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+},
+  // ...
+
+
+  logoutuser: function (username, password) {
+    firebase.auth().signOut().then(function () {
+      // Sign-out successful.
+    }).catch(function (error) {
+      // An error happened.
+    })
+  }
+}
+
+$("#submitBtn").on("click", function(e){
+  e.preventDefault();
+  console.log(e);
+  var uUsername = $('#emailInput').val().trim();
+  console.log(typeof uUsername);
+  var uPass = $('#passwordInput').val();
+  console.log(typeof uPass);
+
+  fbauth.createUser(uUsername, uPass);
+  fbauth.signIn(uUsername, uPass);
+});
+
+$("#login").on("click", function(e){
+  e.preventDefault();
+  var username = $('#exampleInputEmail1').val().trim();
+  var password = $('#exampleInputPassword1').val();
+  fbauth.signIn(username, password);
+  console.log('You signed in');
 })
-
-
-
-
-//processy
-
-
-
